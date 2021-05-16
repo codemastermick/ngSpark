9f7d6d927b555e75f18c63bfd326c3e9842db2ea
Mick Evans <codemastermick@gmail.com>
2021-05-16 14:02:25 -0400

    chore: add dependencies for Firebase

---
+++ EXAMPLE.config.js
@@ -0,0 +1,10 @@
+// RENAME THIS FILE TO firebase.config.js after filling in your project configuration
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "PROJECT-ID.firebaseapp.com",
+  projectId: "PROJECT-ID",
+  storageBucket: "PROJECT-ID.appspot.com",
+  messagingSenderId: "YOUR-SENDER-ID",
+  appId: "YOUR-APP-ID",
+  measurementId: "YOUR-MEASUREMENT-ID"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+
+import { firebaseConfig } from "../../firebase.config";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
