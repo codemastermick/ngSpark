8073046f18ba5ad9346ff084b6243a39036f29e2
Mick Evans <codemastermick@gmail.com>
2021-05-16 15:03:30 -0400

    feat: fill out verify email page

--- src/app/auth/verify-email/verify-email.component.ts
+++ src/app/auth/verify-email/verify-email.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.scss']
+  styleUrls: ['./verify-email.component.scss'],
 })
 export class VerifyEmailComponent implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
